5b9837d98e013d27d9522b24a6e715e544ee057c
Joey Galvin <joeygalvin2134@gmail.com>
2026-02-05 23:24:07 +0000

    refactored solution checker into method and return a basic fitness score

--- Utils.py
+++ Utils.py
@@ -1,13 +1,4 @@
 def read_instance(filename):
-    """
-    Parse the exam timetabling input file.
-
-    Returns:
-        N: number of exams
-        K: number of time slots
-        M: number of students
-        matrix: M x N matrix (matrix[student][exam])
-    """
     with open(filename, 'r') as file:
         first_line = file.readline().strip().split()
         N = int(first_line[0])
@@ -20,3 +11,19 @@ def read_instance(filename):
             matrix.append([int(x) for x in row])
 
     return N, K, M, matrix
+
+def check_solution(solution, student_matrix):
+    violations = 0
+    for (i, row) in enumerate(student_matrix):
+        student_exams = [j for j, exam in enumerate(row) if exam == 1]
+        student_timeslots = [solution[exam] for exam in student_exams]
+        print(
+            f"Student {i:2d} is enrolled in exams: {student_exams} which take place in timeslots: {student_timeslots}")
+
+        if len(student_timeslots) != len(set(student_timeslots)):
+            print(f"Student {i:2d} has a hard constraint violation")  # example: Student2 exams: [0, 4, 6] in timeslots: [1, 1, 3], violation
+            violations += 1
+        else:
+            print(f"Student {i:2d} has no conflicts")  # example: Student0 exams: [5, 6, 7] in timeslots: [2, 3, 4], no violation
+
+    return len(student_matrix) - violations # number of students without hard constraint violations, higher the better
