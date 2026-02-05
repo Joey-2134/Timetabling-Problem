22ffb9675c0c290a4f9f818d60b74c3b77da387e
Joey Galvin <joeygalvin2134@gmail.com>
2026-02-05 23:56:50 +0000

    added method for initalizing a population

--- Utils.py
+++ Utils.py
@@ -1,3 +1,5 @@
+import random
+
 def read_instance(filename):
     with open(filename, 'r') as file:
         first_line = file.readline().strip().split()
@@ -17,13 +19,21 @@ def check_solution(solution, student_matrix):
     for (i, row) in enumerate(student_matrix):
         student_exams = [j for j, exam in enumerate(row) if exam == 1]
         student_timeslots = [solution[exam] for exam in student_exams]
-        print(
-            f"Student {i:2d} is enrolled in exams: {student_exams} which take place in timeslots: {student_timeslots}")
+        # print(f"Student {i:2d} is enrolled in exams: {student_exams} which take place in timeslots: {student_timeslots}")
 
         if len(student_timeslots) != len(set(student_timeslots)):
-            print(f"Student {i:2d} has a hard constraint violation")  # example: Student2 exams: [0, 4, 6] in timeslots: [1, 1, 3], violation
+            # print(f"Student {i:2d} has a hard constraint violation")  # example: Student2 exams: [0, 4, 6] in timeslots: [1, 1, 3], violation
             violations += 1
-        else:
-            print(f"Student {i:2d} has no conflicts")  # example: Student0 exams: [5, 6, 7] in timeslots: [2, 3, 4], no violation
+        # else:
+        #     print(f"Student {i:2d} has no conflicts")  # example: Student0 exams: [5, 6, 7] in timeslots: [2, 3, 4], no violation
 
     return len(student_matrix) - violations # number of students without hard constraint violations, higher the better
+
+def initialize_population(population_size, num_exams, num_timeslots):
+    population = []
+    for solution in range(population_size):
+        solution = []
+        for j in range(num_exams):
+            solution.append(random.randint(0, num_timeslots - 1))
+        population.append(solution)
+    return population
