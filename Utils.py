4ad7ade35cb4ee7a5e1096a35d640a0a22f71c99
Joey Galvin <joeygalvin2134@gmail.com>
2026-02-06 12:53:54 +0000

    mutation, fitness over generation plotted

--- Utils.py
+++ Utils.py
@@ -37,3 +37,41 @@ def initialize_population(population_size, num_exams, num_timeslots):
             solution.append(random.randint(0, num_timeslots - 1))
         population.append(solution)
     return population
+
+def generate_next_generation(population, student_matrix, pop_size, tournament_size, crossover_rate, mutation_rate, num_timeslots, elite_percentage):
+    next_gen_population = []
+    fitnesses = [check_solution(solution, student_matrix) for solution in population]
+
+    # Elitism - Keep the top elite_percentage of solutions
+    num_elites = max(1, int(pop_size * elite_percentage))
+    sorted_indices = sorted(range(pop_size), key=lambda i: fitnesses[i], reverse=True)
+    for i in range(num_elites):
+        next_gen_population.append(population[sorted_indices[i]])
+
+    # Tournament selection and Crossover
+    while len(next_gen_population) < pop_size:
+        parents = []
+        for _ in range(2):
+            randIdxs = [random.randint(0, pop_size - 1) for _ in range(tournament_size)]
+            tournament_fitnesses = []
+            for idx in randIdxs:
+                tournament_fitnesses.append(fitnesses[idx])
+            best_selected_index = tournament_fitnesses.index(max(tournament_fitnesses))
+            parents.append(population[randIdxs[best_selected_index]])
+
+        split_idx = random.randint(1, len(parents[0]) - 1)  # index where we split for crossover
+        if random.random() < crossover_rate:
+            child1 = parents[0][:split_idx] + parents[1][split_idx:]
+            child2 = parents[1][:split_idx] + parents[0][split_idx:]
+        else:
+            child1, child2 = parents[0][:], parents[1][:]
+
+        # Mutation
+        for child in [child1, child2]:
+            for i in range(len(child)):
+                if random.random() < mutation_rate:
+                    child[i] = random.randint(0, num_timeslots - 1)
+
+        next_gen_population.extend([child1, child2])
+
+    return next_gen_population
